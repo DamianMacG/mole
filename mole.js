21ad2b95ee4156b3d0c765469e1255157f409a0f
Damian <damiangildea12@gmail.com>
2023-06-08 15:15:50 +0100

    Completed task 2

--- mole.js
+++ mole.js
@@ -1,4 +1,5 @@
 const https = require("node:https");
+const fs = require("fs");
 
 function getPeople() {
   const search = {
@@ -15,28 +16,50 @@ function getPeople() {
 
     response.on("end", () => {
       const parsedBody = JSON.parse(body);
-      console.log(parsedBody.job);
       const people = parsedBody.people;
       const workers = people.filter(
         (work) => work.job.workplace === "northcoders"
       );
-      console.log(workers);
+      // console.log(workers);
     });
   });
+
   request.end();
 }
 
-getPeople();
+function getInterests() {
+  const jsonData = fs.readFileSync("northcoders.json", "utf8");
+  const data = JSON.parse(jsonData);
+  const usernames = data.map((user) => user.username);
 
-// Haz Mallion
-// Paul Copley
-// Sam Caine
-// Vel Georgieva
-// Sarah Browncross
-// Liam Duncan
-// Ant Medina
-// Lucy Galway
-// Paul Rogerson
-// Ruth Ng
-// Amy Wild
-// David Bartlett
+  for (let i = 0; i < usernames.length; i++) {
+    const search = {
+      host: "nc-leaks.herokuapp.com",
+      path: `/api/people/${usernames[i]}/interests`,
+      method: "GET",
+    };
+    const request = https.request(search, (response) => {
+      let body = "";
+      response.on("data", (packet) => {
+        body += packet.toString();
+      });
+      response.on("end", () => {
+        const parsedBody = JSON.parse(body);
+        // console.log(parsedBody);
+        const interests = parsedBody.person.interests
+        console.log(interests)
+      });
+    });
+    request.end();
+  }
+}
+
+function getPets() {
+  const search = {
+    host: "nc-leaks.herokuapp.com",
+    path: `/api/people/`,
+    method: "GET",
+  };
+}
+
+getInterests();
