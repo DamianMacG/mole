b5be07f5d68b5f56795adde340c365c80d815df3
Damian <damiangildea12@gmail.com>
2023-06-08 16:30:00 +0100

    Completed Task 3

--- mole.js
+++ mole.js
@@ -1,5 +1,6 @@
 const https = require("node:https");
 const fs = require("fs");
+const { type } = require("node:os");
 
 function getPeople() {
   const search = {
@@ -20,7 +21,8 @@ function getPeople() {
       const workers = people.filter(
         (work) => work.job.workplace === "northcoders"
       );
-      // console.log(workers);
+      let data = JSON.stringify(workers, null, 2);
+      fs.writeFileSync("northcoders.json", data);
     });
   });
 
@@ -31,7 +33,7 @@ function getInterests() {
   const jsonData = fs.readFileSync("northcoders.json", "utf8");
   const data = JSON.parse(jsonData);
   const usernames = data.map((user) => user.username);
-
+  let array = [];
   for (let i = 0; i < usernames.length; i++) {
     const search = {
       host: "nc-leaks.herokuapp.com",
@@ -45,9 +47,8 @@ function getInterests() {
       });
       response.on("end", () => {
         const parsedBody = JSON.parse(body);
-        // console.log(parsedBody);
-        const interests = parsedBody.person.interests
-        console.log(interests)
+        array.push(parsedBody);
+        fs.writeFileSync("interests.json", JSON.stringify(array, null, 2));
       });
     });
     request.end();
@@ -55,11 +56,35 @@ function getInterests() {
 }
 
 function getPets() {
-  const search = {
-    host: "nc-leaks.herokuapp.com",
-    path: `/api/people/`,
-    method: "GET",
-  };
+  const jsonData = fs.readFileSync("northcoders.json", "utf8");
+  const data = JSON.parse(jsonData);
+  const usernames = data.map((user) => user.username);
+  let array = [];
+
+  for (let i = 0; i < usernames.length; i++) {
+    const search = {
+      host: "nc-leaks.herokuapp.com",
+      path: `/api/people/${usernames[i]}/pets`,
+      method: "GET",
+    };
+    const request = https.request(search, (response) => {
+      let body = "";
+      response.on("data", (packet) => {
+        body += packet.toString();
+      });
+      response.on("end", () => {
+        const parsedBody = JSON.parse(body);
+        if (parsedBody.status !== 404) {
+          array.push(parsedBody);
+        }
+        const content = JSON.stringify(array, null, 2);
+        fs.writeFileSync("pets.json", content);
+      });
+    });
+    request.end();
+  }
 }
 
+getPets();
 getInterests();
+getPeople();
